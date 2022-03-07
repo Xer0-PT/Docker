bf6f05f06d7b38252356e18c69b7a48f0168d87a
Joel Martins <38245877+Xer0-PT@users.noreply.github.com>
2022-03-07 16:08:37 +0000

    init

---
+++ run-docker-services.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+ECHO.
+ECHO ========================
+ECHO Starting Docker Services
+ECHO ========================
+ECHO.
+docker-compose -f .\docker-compose-infrastructure.yml -f .\docker-compose-infrastructure.override.yml up
